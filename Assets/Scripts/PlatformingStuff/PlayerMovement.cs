6ee84238ef6c50b64e725ac9e7dc15bea56920d0
Michael Demone <m_d_emone@hotmail.com>
2019-08-09 10:36:37 -0700

    Added dashing to the player.

--- Assets/Scripts/PlatformingStuff/PlayerMovement.cs
+++ Assets/Scripts/PlatformingStuff/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using G4AW2.Utils;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -8,19 +9,32 @@ public class PlayerMovement : MonoBehaviour {
 
     public static PlayerMovement Instance;
 
-    public float MovementForceStrength = 10f;
 
+    [Header("Autoset References")]
     [AutoSet] public Rigidbody2D body;
     [AutoSet(SetByNameInChildren = true)] public SpriteRenderer View;
-
     [AutoSet] public Transform t;
-
     [AutoSet(CheckChildrenForComponents = true)] public PlayerAnimations Animations;
     [AutoSet(CheckChildrenForComponents = true, SetByNameInChildren = true)] public Transform Weapon;
 
+    [Header("Movement")]
+    public float MovementForceStrength = 1f;
 
+    [Header("Weapon")]
     public float WeaponDistanceFromPlayer = 0.15f;
     public Vector3 WeaponOffset = new Vector2(0, 0.05f);
+
+    [Header("Dash")]
+    public float DashForceStrength = 3f;
+    public float MaxTimeBetweenButtonPushesForDash = 0.5f;
+    public float DashingDuration = 0.1f;
+    public float DashCooldownDuration = 0.4f;
+
+    private bool dashing = false;
+    private Vector2 dashDirection;
+    private float dashingDurationEnd = 0;
+    private float canDashAgainTime = 0;
+    private bool CanDash => Time.time > canDashAgainTime;
 
     private bool attacking = false;
 
@@ -40,21 +54,52 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+
         // Set Velocity
+        Vector3 vel;
         {
-            Vector2 vel = new Vector2();
-            if(Input.GetKey(KeyCode.D)) {
-                vel.x = 1;
+            if (dashing) {
+                if (dashingDurationEnd <= Time.time) dashing = false;
+                vel = dashDirection * DashForceStrength;
             }
-            if(Input.GetKey(KeyCode.A)) {
-                vel.x += -1;
+            else {
+
+                // Dash Checks
+                if (CanDash) {
+                    if(Input.GetKeyDown(KeyCode.D)) {
+                        StartCoroutine(CheckDash(KeyCode.D));
+                    }
+                    if(Input.GetKeyDown(KeyCode.A)) {
+                        StartCoroutine(CheckDash(KeyCode.A));
+                    }
+                    if(Input.GetKeyDown(KeyCode.W)) {
+                        StartCoroutine(CheckDash(KeyCode.W));
+                    }
+                    if(Input.GetKeyDown(KeyCode.S)) {
+                        StartCoroutine(CheckDash(KeyCode.S));
+                    }
+                }
+
+                vel = new Vector2();
+                if(Input.GetKey(KeyCode.D)) {
+                    vel.x = 1;
+                }
+                if(Input.GetKey(KeyCode.A)) {
+                    vel.x += -1;
+                }
+                if(Input.GetKey(KeyCode.W)) {
+                    vel.y = 1;
+                }
+                if(Input.GetKey(KeyCode.S)) {
+                    vel.y += -1;
+                }
+
+                vel = vel.normalized * MovementForceStrength;
             }
-            if(Input.GetKey(KeyCode.W)) {
-                vel.y = 1;
-            }
-            if(Input.GetKey(KeyCode.S)) {
-                vel.y += -1;
-            }
+        }
+
+        // Set Walking Animations
+        {
             if(body.velocity.magnitude == 0 && vel.magnitude > 0) {
                 // Started walking
                 Animations.StartWalking();
@@ -63,7 +108,8 @@ public class PlayerMovement : MonoBehaviour {
                 // Stopped walking
                 Animations.StopWalking();
             }
-            body.velocity = vel.normalized * MovementForceStrength;
+
+            body.velocity = vel;
         }
 
         // Set attack animations
@@ -108,6 +154,62 @@ public class PlayerMovement : MonoBehaviour {
 
 
     }
+
+    #region Dashing
+
+
+    IEnumerator CheckDash(KeyCode key) {
+        float time = Time.time;
+
+
+        // Key down -> Key up -> key down is a double tap 
+        if(!Input.GetKeyDown(key))
+            yield break;
+
+        while(Time.time < time + MaxTimeBetweenButtonPushesForDash) {
+
+            if(Input.GetKeyUp(key)) {
+                break;
+            }
+
+            yield return null;
+        }
+
+        while(Time.time < time + MaxTimeBetweenButtonPushesForDash) {
+
+            if(Input.GetKeyDown(key)) {
+                break;
+            }
+
+            yield return null;
+        }
+
+        if(Time.time >= time + MaxTimeBetweenButtonPushesForDash || dashing)
+            yield break;
+
+        dashing = true;
+
+        // Successful dash!
+        if(key == KeyCode.A) {
+            // Dash left
+            dashDirection = Vector2.left;
+        } else if(key == KeyCode.S) {
+            // Dash down
+            dashDirection = Vector2.down;
+        } else if(key == KeyCode.D) {
+            // Dash right
+            dashDirection = Vector2.right;
+        } else if(key == KeyCode.W) {
+            // Dash up
+            dashDirection = Vector2.up;
+        }
+
+        dashingDurationEnd = Time.time + DashingDuration;
+        canDashAgainTime = Time.time + DashCooldownDuration;
+    }
+
+    #endregion
+
 
     private void OnCollisionEnter2D(Collision2D collision) {
 
